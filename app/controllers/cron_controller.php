bb9943e6bef01d108c5af8dbfa8981466e95cdfa
jon.huang <sakura.speed@gmail.com>
2013-03-28 23:08:12 +0800

    修正slice取值错误

--- app/controllers/cron_controller.php
+++ app/controllers/cron_controller.php
@@ -37,12 +37,15 @@ class CronController extends AppController {
 
         if ($type == 'fanli') {
 
-            $users = $this->UserFanli->findAll(array('role' => array(1, 2, 3), 'status' => array(1, 3)));
+            //$users = $this->UserFanli->findAll(array('role' => array(1, 2, 3), 'status' => array(1, 3)));
+            $weekdate = date('Y-m-d', time()-7*24*3600);
+            $users = $this->UserFanli->findAll("(role IN(1,2,3) AND status IN(1,3)) OR (status = 2 AND ts > '{$weekdate}')");
+            
             $userids = fieldSet($users, 'userid');
             //对用户进行分段，每50个一组
             $page = ceil(count($userids) / 50);
             for ($i = 0; $i < $page; $i++) {
-                $new = array_slice($userids, $i*2, 50);
+                $new = array_slice($userids, $i*50, 50);
                 $ids = join($new, ',');
                 $p = array();
                 $p['userid'] = $ids;

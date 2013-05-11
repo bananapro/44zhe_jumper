243356a64126a4439ae721ddad1601480dc76709
jon.huang <sakura.speed@gmail.com>
2013-05-11 23:57:20 +0800

    去除特殊额地区限制

--- app/controllers/api_controller.php
+++ app/controllers/api_controller.php
@@ -269,7 +269,8 @@ class ApiController extends AppController {
 			$data = json_decode($data, true);
 			if ($data['status']) {
 				$_GET['ju'] = $data['data']['url'];
-				$_GET['p_seller'] = $data['data']['shopname'];;
+				$_GET['p_seller'] = $data['data']['shopname'];
+				;
 				$_GET['p_title'] = $data['data']['title'];
 			}
 			else {
@@ -319,26 +320,28 @@ class ApiController extends AppController {
 		//改良算法，小于3元跳推荐，12天后无推荐成功恢复推手身份
 		if ($p_fanli <= 3.5) {
 
-			if($p_price >= 30){
+			if ($p_price >= 30) {
 
 				//专门用来测试返利网模式跳转
-				if ($my_user == 'bluecone@163.com' && getAreaByIp()=='上海') {
+				if ($my_user == 'bluecone@163.com' && getAreaByIp() == '上海') {
 					$user = $this->UserFanli->getPoolSpan('辽宁');
-				}else{
+				}
+				else {
 					$user = $this->UserFanli->getPoolSpan();
 				}
 
-                if(!$user){//尽量找到
+				if (!$user) {//尽量找到
 					$user = $this->UserFanli->getPoolSpan('辽宁');
-                    if(!$user)alert('Pool Error', 'level 1 empty');
-                }
-
-			}else{
+					if (!$user)
+						alert('Pool Error', 'level 1 empty');
+				}
+			}else {
 				$user = $this->UserFanli->getPoolSpan('', 2);
-                if(!$user){//尽量找到
+				if (!$user) {//尽量找到
 					$user = $this->UserFanli->getPoolSpan('辽宁', 2);
-                    if(!$user)alert('Pool Error', 'level 2 empty');
-                }
+					if (!$user)
+						alert('Pool Error', 'level 2 empty');
+				}
 			}
 
 			if ($user) {
@@ -350,17 +353,16 @@ class ApiController extends AppController {
 					$this->redirect('/api/nojobs');
 				}
 			}
-		}else{
+		}
+		else {
 			//跳入特殊账号，直到超过累计值
 			$area = getAreaByIp();
-			if ($area == '辽宁') {
-				if (!overlimit_day('SP_FANLI_MAX', date('Ym'))) {
-					$r = rand(0, 1); //每月1号以前几率递增
-					if ($r < date('d')) {
-						$user = array();
-						$user['userid'] = C('config', 'SP_UID');
-						overlimit_day_incr('SP_FANLI_MAX', date('Ym'), $p_fanli);
-					}
+			if (!overlimit_day('SP_FANLI_MAX', date('Ym'))) {
+				$r = rand(0, 1); //每月1号以前几率递增
+				if ($r < date('d')) {
+					$user = array();
+					$user['userid'] = C('config', 'SP_UID');
+					overlimit_day_incr('SP_FANLI_MAX', date('Ym'), $p_fanli);
 				}
 			}
 		}
@@ -431,7 +433,7 @@ class ApiController extends AppController {
 			$user = $this->UserMizhe->getUser('辽宁');
 		}
 
-		if(!$user){
+		if (!$user) {
 			$this->jumpForce($shop, $my_user, $p_id, $p_price, $p_fanli);
 		}
 

f6075e717c73d63e9a76ec70c1bf581decbd46e8
jon.huang <sakura.speed@gmail.com>
2013-05-27 01:13:43 +0800

    优化隐藏逻辑

--- app/webroot/js/h.js
+++ app/webroot/js/h.js
@@ -20,18 +20,21 @@ for(var i = 0; i < pairs.length; i++) {
 
 if(myhost == 'fun.51fanli.com' && myhref.indexOf('goshopapi')>0){
 
-    var b = document.getElementsByTagName('div');
     document.getElementsByTagName('div')[0].style.display='none';
+    document.getElementsByTagName('div')[1].style.display='none';
 
 }else if(myhost == 'go.mizhe.com' && myhref.indexOf('GdSNEtWbTlBY1FpcEt3UXplUE9lRURyWVZWYTY0eUs4Q2NrZmY3VFZSQW')>0){
 
-    document.getElementById('cboxWrapper').style.display='none';
+    ;
+    document.getElementsByTagName('iframe')[0].src='about:blank';
+    document.getElementById('colorbox').style.display='none';
+
+    setInterval("document.getElementById('colorbox').style.display='none'",100);
+    setInterval("document.getElementsByTagName('cboxWrapper')[0].src='about:blank'",100);
+
     if(args['p_id'] && args['p_id'] != 'undefined' ){
 
 	var referLink = document.createElement('a');
-	var b = document.getElementsByTagName('div');
-	document.getElementsByTagName('div')[0].style.display='none';
-
 	referLink.href = 'http://go.44zhe.com/api/jumpMizhe/'+args['shop']+'/'+args['my_user']+'/'+args['p_id']+'/'+args['p_price']+'/'+args['p_fanli']+'?&oc='+args['oc']+'&target='+args['target'];
 	document.body.appendChild(referLink);
 	referLink.click();

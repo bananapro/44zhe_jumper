5420d9542a2807d48287320ff8265f66ef9657c8
jon.huang <sakura.speed@gmail.com>
2013-10-06 16:06:46 +0800

    调整参数

--- app/config/myconfig/config.php
+++ app/config/myconfig/config.php
@@ -35,7 +35,7 @@
 		'HOLD_SELLER' => array('bluecone'),
 
 		//向阿雄结算费率
-		'RATE' => 0.30,
+		'RATE' => 0.35,
 
 		//米折网返利折扣，用于跟单还原原始佣金
 		'RATE_MIZHE' => 55,
@@ -61,6 +61,6 @@
 		'JUMP_CHANNEL_ENABLE' => array('geihui', 'baobeisha', 'jsfanli', 'fanxian', 'taofen8'),
 
 		//渠道会员绑定插件账号数量上限
-		'JUMP_CHANNEL_BIND_LIMIT' => 25,
+		'JUMP_CHANNEL_BIND_LIMIT' => 20,
     )
 ?>

bb39e16b37d1c43cf4e6360ca7113503a8658035
jon.huang <sakura.speed@gmail.com>
2013-10-17 23:42:37 +0800

    sms code 输入页面如果获取不到cookie信息则提示错误

--- app/controllers/job_controller.php
+++ app/controllers/job_controller.php
@@ -68,23 +68,32 @@ class JobController extends AppController {
 	function smsCode(){
 
 		$flush = true;
+		$error = false;
 		//提交数据设置任务
 		if(@$_POST['mobile']){
 			//清除mobile code值
 			$sid = $_COOKIE['PHPSESSID'];
 			$uid = $_COOKIE['prouserid'];
-			$this->SmsCode->save(array('id'=>1, 'mobile'=>$_POST['mobile'], 'sid'=>$sid, 'uid'=>$uid, 'code'=>''));
+			if(!$sid || !$uid){
+				$error = true;
+			}else{
+				$this->SmsCode->save(array('id'=>1, 'mobile'=>$_POST['mobile'], 'sid'=>$sid, 'uid'=>$uid, 'code'=>''));
+			}
 		}
 
 		$hit_code = $this->SmsCode->find("id=1 AND code <> ''");
 
-		if($hit_code){
+		if($hit_code && !$error){
 			//不再刷新页面
 			clearTableName($hit_code);
 			$flush = false;
 			$this->set('code', $hit_code);
+		}else if($error){
+			$flush = false;
+			$this->set('code', false);
 		}
 
+		$this->set('error', $error);
 		$this->set('flush', $flush);
 	}
 

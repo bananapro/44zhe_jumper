5d07cccc85c1815759f958319c02c01ffc3f32a5
jon.huang <sakura.speed@gmail.com>
2013-10-26 16:04:57 +0800

    提高渠道账号绑定用户数

--- app/config/myconfig/config.php
+++ app/config/myconfig/config.php
@@ -71,6 +71,6 @@
 		'JUMP_CHANNEL_ENABLE' => array('geihui', 'baobeisha', 'jsfanli', 'fanxian', 'flk123'),
 
 		//渠道会员绑定插件账号数量上限
-		'JUMP_CHANNEL_BIND_LIMIT' => 20,
+		'JUMP_CHANNEL_BIND_LIMIT' => 25,
     )
 ?>

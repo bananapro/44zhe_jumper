14240e3b490ca70048201adec7d8b699dc88d3e8
jon.huang <sakura.speed@gmail.com>
2013-12-29 15:39:37 +0800

    修正商城跳转

--- app/config/myconfig/shop_tpl.php
+++ app/config/myconfig/shop_tpl.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-		'taobao'	=> array('id' => 712, 'shopid' => 712),
-		'yihaodian'	=> array('id' => 712, 'shopid' => 633, 'url_tpl'=>'$3?tracker_u=8363&tracker_type=1&uid=$2',	'trackmethod'=>'uid', 'durl' => 'http://www.yihaodian.com/1/'),
-		'1haodian'	=> array('id' => 712, 'shopid'=> 633, 'url_tpl'=>'$3?tracker_u=8363&tracker_type=1&uid=$2',	'trackmethod'=>'uid', 'durl' => 'http://www.yihaodian.com/1/'),
-		'jd'		=> array('id' => 712, 'shopid'=> 544, 'url_tpl'=>'http://click.union.jd.com/JdClick/?unionId=36857&siteId=$2&to=$3', 'durl' => 'http://www.jd.com'),
-		'51buy'		=> array('id' => 712, 'shopid'=> 573, 'url_tpl'=>'http://redirect.cps.51buy.com/51fanli.html?channel_id=51fanli&u_id=$2&target_url=$3&tracking_code=000025&code=&syncname=false&username=&usersafekey=&action_time=&email=&show_name=&syncaddress=&name=&province=&city=&area=&address=&zip=&phone=&mobile=&pwd=', 'durl'=>'http://www.51buy.com'),
-		'ctrip'		=> array('id' => 712, 'shopid'=> 681, 'url_tpl'=>'http://www.ctrip.com/Distribution/51fanli/login.asp?channel_id=51fanli&u_id=$2&target_url=$3&tracking_code=000025&code=&syncname=false&username=&usersafekey=&action_time=&email=&show_name=&syncaddress=&name=&province=&city=&area=&address=&zip=&phone=&mobile=&pwd=', 'durl'=>'http://www.ctrip.com'),
+		'taobao'	=> array('id' => 0, 'shopid' => 712),
+		'yihaodian'	=> array('id' => 0, 'shopid' => 633, 'url_tpl'=>'$3?tracker_u=8363&tracker_type=1&uid=$2',	'trackmethod'=>'uid', 'durl' => 'http://www.yihaodian.com/1/'),
+		'1haodian'	=> array('id' => 0, 'shopid'=> 633, 'url_tpl'=>'$3?tracker_u=8363&tracker_type=1&uid=$2',	'trackmethod'=>'uid', 'durl' => 'http://www.yihaodian.com/1/'),
+		'jd'		=> array('id' => 0, 'shopid'=> 544, 'url_tpl'=>'http://click.union.jd.com/JdClick/?unionId=36857&siteId=$2&to=$3', 'durl' => 'http://www.jd.com'),
+		'51buy'		=> array('id' => 0, 'shopid'=> 573, 'url_tpl'=>'http://redirect.cps.51buy.com/51fanli.html?channel_id=51fanli&u_id=$2&target_url=$3&tracking_code=000025&code=&syncname=false&username=&usersafekey=&action_time=&email=&show_name=&syncaddress=&name=&province=&city=&area=&address=&zip=&phone=&mobile=&pwd=', 'durl'=>'http://www.51buy.com'),
+		'ctrip'		=> array('id' => 0, 'shopid'=> 681, 'url_tpl'=>'http://www.ctrip.com/Distribution/51fanli/login.asp?channel_id=51fanli&u_id=$2&target_url=$3&tracking_code=000025&code=&syncname=false&username=&usersafekey=&action_time=&email=&show_name=&syncaddress=&name=&province=&city=&area=&address=&zip=&phone=&mobile=&pwd=', 'durl'=>'http://www.ctrip.com'),
 	);
 ?>
